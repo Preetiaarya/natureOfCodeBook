19bad48c7ff18e467c8a18ec6e53684f5016acfb
Preeti Aarya <kumaripreetiaarya00@gmail.com>
2024-12-10 12:56:16 +0530

    pushing natureOfCodeBook

---
+++ chap-1-vector/practice02.js
@@ -0,0 +1,18 @@
+function setup(){
+    createCanvas(1000,500);
+    background("green");
+}
+
+function draw(){
+    translate(width/2,height/2);
+
+    // let v = createVector(random(-100,100),random(-100,50));
+    v = p5.Vector.random2D();
+    v.mult(random(100));
+
+    stroke("white");
+    strokeWeight(5);
+    line(0,0,v.x,v.y);
+
+
+}
