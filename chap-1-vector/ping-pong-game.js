2593bbd71f579d59b35987960567283b42fc6915
Preeti Aarya <kumaripreetiaarya00@gmail.com>
2024-12-13 15:37:45 +0530

    ball collides with ball

--- chap-1-vector/ping-pong-game.js
+++ chap-1-vector/ping-pong-game.js
@@ -1,3 +1,37 @@
+// let position;
+// let velocity;
+
+// function setup() {
+//     createCanvas(800, 800);
+
+//     position = createVector(100, 100);
+//     velocity = createVector(2.5, 4);
+// }
+
+// function draw() {
+//     background("pink");
+
+//     position.add(velocity);
+
+//     if (position.x > width|| position.x < 0) {
+//         velocity.x = velocity.x * - 1;
+
+//     }
+//     if (position.y > height || position.y <0) {
+//         velocity.y = velocity.y * - 1;
+//     }
+//     fill("black");
+//     stroke("white");
+//     strokeWeight(4);
+//     circle(position.x, position.y, 50);
+
+//     fill("green")
+   
+//    rect(0,position.y,20,200);
+//    rect(780,mouseY,20,200);
+
+// }
+
 let position;
 let velocity;
 
@@ -9,26 +43,53 @@ function setup() {
 }
 
 function draw() {
-    background("pink");
+    background(153,76,0);
 
     position.add(velocity);
 
+    // Wall collision logic
     if (position.x > width || position.x < 0) {
-        velocity.x = velocity.x * - 1;
-
+        velocity.x *= -1;
     }
     if (position.y > height || position.y < 0) {
-        velocity.y = velocity.y * - 3;
+        velocity.y *= -1;
     }
+
+    // Draw ball
     fill("black");
     stroke("white");
     strokeWeight(4);
     circle(position.x, position.y, 50);
 
-    fill("green")
-   rect(780,mouseY,20,200);
-   rect(0,300,20,200);
+    // Draw paddles
+    fill("green");
+    let paddleHeight = 200;
+    let paddleWidth = 20;
+    let leftPaddleY = position.y - paddleHeight / 2;
+    let rightPaddleY = mouseY - paddleHeight / 2;
 
+    rect(0, leftPaddleY, paddleWidth, paddleHeight);
+    rect(width - paddleWidth, rightPaddleY, paddleWidth, paddleHeight);
+
+    // Collision detection for left paddle
+    if (
+        position.x - 25 <= paddleWidth && // Ball touching the left paddle
+        position.y + 25 >= leftPaddleY &&
+        position.y - 25 <= leftPaddleY + paddleHeight
+    ) {
+        velocity.x *= -1; // Reverse x direction
+        position.x = paddleWidth + 25; // Adjust ball position to avoid sticking
+    }
+
+    // Collision detection for right paddle
+    if (
+        position.x + 25 >= width - paddleWidth && // Ball touching the right paddle
+        position.y + 25 >= rightPaddleY &&
+        position.y - 25 <= rightPaddleY + paddleHeight
+    ) {
+        velocity.x *= -1; // Reverse x direction
+        position.x = width - paddleWidth - 25; // Adjust ball position to avoid sticking
+    }
 }
 
 
