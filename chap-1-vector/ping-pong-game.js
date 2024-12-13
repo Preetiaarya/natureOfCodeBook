f352d2eb76716aa8bf95cbb33bbe34c97be23b06
Preeti Aarya <kumaripreetiaarya00@gmail.com>
2024-12-13 12:23:25 +0530

    change rect Y position

--- chap-1-vector/ping-pong-game.js
+++ chap-1-vector/ping-pong-game.js
@@ -1,7 +1,5 @@
 let position;
 let velocity;
-let mouseX;
-let mouseY;
 
 function setup() {
     createCanvas(800, 800);
@@ -20,7 +18,7 @@ function draw() {
 
     }
     if (position.y > height || position.y < 0) {
-        velocity.y = velocity.y * - 1;
+        velocity.y = velocity.y * - 3;
     }
     fill("black");
     stroke("white");
@@ -28,10 +26,8 @@ function draw() {
     circle(position.x, position.y, 50);
 
     fill("green")
-    rect(0, 200, 50, 400);
-    //   rect(750,200,50,400);
-    fill("blue");
-    rect(mouseX - 25, mouseY - 25, 50, 50);
+   rect(780,mouseY,20,200);
+   rect(0,300,20,200);
 
 }
 
@@ -50,7 +46,6 @@ function draw() {
 
 // function setup() {
 //     createCanvas(800, 800);
-//     // background("yellow");
 
 //     mover = new Mover();
 // }
@@ -64,12 +59,12 @@ function draw() {
 // class Mover {
 //     constructor() {
 //         this.position = createVector(width / 2, height / 2);
-//         this.velocity = createVector(2.5, 4);
+//         this.velocity = createVector(2.5, 3);
 //     }
 //     update() {
 //         let mouse = createVector(mouseX, mouseY);
 
-//         // this.position.add(this.velocity);
+//         this.position.add(this.velocity);
 
 //     }
 //     show() {
@@ -79,8 +74,8 @@ function draw() {
 //         circle(this.position.x, this.position.y, 50);
 
 //         fill("green");
-//         rect(0,200,50,400);
-//         rect(750,200,50,400);
+//         rect(0,220,20,300);
+//         rect(780,mouseY,50,300);
 //     }
 // }
 
