f3677d8ffc8966d699bd5c1833f8d2c07a88f4fe
Preeti Aarya <kumaripreetiaarya00@gmail.com>
2024-12-30 12:28:39 +0530

    create backgroung,helium,wind in draw function

--- chap-2-force/forceAccumulation.js
+++ chap-2-force/forceAccumulation.js
@@ -3,8 +3,28 @@ let balloon;
 
 function setup() {
   createCanvas(1200, 800);
-  balloon = new Balloon(400, 500,1); // Initial position and mass
+  balloon = new Balloon(400, 500,1);
 }
+function draw() {
+    background(0, 153, 153);
+  
+    // Upward force to simulate helium
+    let helium = createVector(0, -0.02);
+  
+    // Wind force based on Perlin noise
+    let windStrength = noise(frameCount * 0.01) * 0.2 - 0.1; // Varies between -0.1 and 0.1
+    
+    let wind = createVector(windStrength, 0);
+  
+    
+    balloon.applyForce(helium);
+    balloon.applyForce(wind);
+  
+    
+    balloon.update();
+    balloon.edges();
+    balloon.show();
+  }
 
 
 
