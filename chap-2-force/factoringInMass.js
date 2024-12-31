35d4c2fbb0b7adab5553ce01604c11f33a603b1d
Preeti Aarya <kumaripreetiaarya00@gmail.com>
2024-12-31 19:11:58 +0530

    add draw function

--- chap-2-force/factoringInMass.js
+++ chap-2-force/factoringInMass.js
@@ -1,9 +1,28 @@
 let moverA;
 let moverB;
 
-function setup(){
-    createCanvas(1200,800);
+function setup() {
+    createCanvas(1200, 800);
 
-    moverA = new Mover(100,200,2);
-    moverB = new Mover(300,200,4);
+    moverA = new Mover(100, 200, 2);
+    moverB = new Mover(300, 200, 4);
+}
+function draw() {
+    background(153, 153, 0);
+
+    if (mouseIsPressed) {
+        let wind = createVector(0.1, 0);
+        moverA.applyForce(wind);
+        moverB.applyForce(wind);
+
+        moverA.update();
+        moverA.edges();
+        moverA.show();
+
+        moverB.update();
+        moverB.edges();
+        mover.show();
+    }
+
+    
 }
