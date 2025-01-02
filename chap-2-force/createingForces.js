b82fe695665ad108694524d859ee1bf93e0d2840
Preeti Aarya <kumaripreetiaarya00@gmail.com>
2025-01-02 12:03:56 +0530

    added update, show or edges function

--- chap-2-force/createingForces.js
+++ chap-2-force/createingForces.js
@@ -11,12 +11,43 @@ background("red");
 
 let gravity = createVector(0,0.1);
 mover.applyForce(gravity);
+
+mover.update();
+mover.show();
+mover.edges();
 }
 
 class Mover{
     constructor(x,y,mass){
 this.pos = createVector(x,y);
 this.vel = createVector(0,0);
+this.acc = createVector(0,0);
+this.mass = mass;
+}
+
+applyForce(force){
+let f = p5.Vector.div(force,this.mass);
+this.acceleration.add(f);
+}
+
+update(){
+    this.pos.add(this.vel);
+    this.vel.add(this.acc);
+    this.set.acc(0,0);
+}
+show(){
+    stroke("white");
+    fill("black");
+    circle(this.pos.x, this.pos.y,this.mass *15);
+}
+
+edges(){
+    if(this.pos.x >= width ||this.pos.x <= 0){
+        this.vel *= -1;
     }
+    else if(this.pos.y >= height || this.pos.y <=0){
+        this.vel *= -1;
+    }
+}
 }
 
