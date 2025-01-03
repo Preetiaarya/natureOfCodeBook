fceb605d5d89396a64054fb84915f98cd02a34c0
Preeti Aarya <kumaripreetiaarya00@gmail.com>
2025-01-03 19:36:33 +0530

    added friction of modeling force

---
+++ chap-2-force/modeling-FrictionExample(2.4).js
@@ -0,0 +1,31 @@
+let mover;
+
+function setup() {
+    createCanvas(1200, 600);
+
+    mover = new Mover(400, 200, 2);
+}
+function draw() {
+    background(224, 224, 224);
+
+    let gravity = createVector(0, 1);
+    mover.applyForce(gravity);
+
+    if (mouseIsPressed) {
+        let wind = createVector(0.5, 0);
+        mover.applyForce(wind);
+    }
+
+    if (mover.contactEdge()) {
+        let c = 0.1;
+        let friction = mover.velocity.copy();
+        friction.mult(-1);
+        friction.setMag(c);
+        // Apply the friction force vector to the object.
+        mover.applyForce(friction);
+    }
+    mover.bounceEdges();
+    mover.update();
+    mover.show();
+
+}
