1de19cfac55c82e2c71666c34bef8e99f2bde269
Preeti Aarya <kumaripreetiaarya00@gmail.com>
2025-01-08 11:55:40 +0530

    added two forces in draw function gravity or wind

--- chap-2-force/modelingForceExe[2.6].js
+++ chap-2-force/modelingForceExe[2.6].js
@@ -7,3 +7,22 @@ function setup(){
     moverA = new Mover(200,200,2);
     moverB = new Mover(300,200,2);
 }
+function draw(){
+    background(220);
+
+    let gravity = createVector(0,0.5);
+    mover.applyForce(gravity);
+
+    if(mouseIsPressed){
+        let wind = createVector(0,0);
+        mover.applyForce(wind);
+    }
+
+    moverA.update();
+    moverA.show();
+    moverA.edges();
+
+    moverB.update();
+    moverB.show();
+    moverB.edges();
+}
