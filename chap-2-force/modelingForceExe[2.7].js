73edecee9e7f9b47e768b086db1a2dd05cf3c3a1
Preeti Aarya <kumaripreetiaarya00@gmail.com>
2025-01-09 10:49:40 +0530

    added mover class to handle mover initial pos,vel,acc and updated methode updating this for each frame

--- chap-2-force/modelingForceExe[2.7].js
+++ chap-2-force/modelingForceExe[2.7].js
@@ -15,4 +15,37 @@ function draw() {
     let gravityB = createVector(0, 0.5);
     moverA.applyForce(gravityA);
     moverB.applyForce(gravityB);
+
+    moverA.updste();
+    moverA.show();
+
+    moverB.update();
+    moverB.show();
+
+}
+
+class Mover{
+    constructor(x,y,mass){
+        this.pos = createVector(x,y);
+        this.vel = createVector(0.5,0);
+        this.acc = createVector(0,0);
+        this.mass = mass;
+    }
+
+    applyForce(force){
+        let f = p5.Vector.div(force,this.mass);
+        this.acc.add(f);
+    }
+
+    update(){
+    this.pos.add(this.vel);
+    this.vel.add(this.acc);
+    this.acc.set(0,0);
+    }
+
+    show(){
+        fill(220);
+        stroke(0);
+        ellipse(this.pos.x,this.pos,y,this.mass *16);
+    }
 }
