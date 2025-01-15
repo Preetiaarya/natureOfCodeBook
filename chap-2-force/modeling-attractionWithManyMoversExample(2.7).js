beda18ed784d471875e26bb28981b8dec7a8642f
Preeti Aarya <kumaripreetiaarya00@gmail.com>
2025-01-16 05:11:15 +0530

    added global variables

--- chap-2-force/modeling-attractionWithManyMoversExample(2.7).js
+++ chap-2-force/modeling-attractionWithManyMoversExample(2.7).js
@@ -1,6 +1,14 @@
-function setup(){
-    createCanvas(400,400);
-}
-function draw(){
+let movers = [];
+let attractor;
+
+function setup() {
+    createCanvas(640, 360);
+    for (let i = 0; i < 10; i++) {
+      movers[i] = new Mover(random(width), random(height), random(0.5, 3));
+    }
+    attractor = new Attractor();
+  }
+  
+function draw() {
     background(220);
 }
