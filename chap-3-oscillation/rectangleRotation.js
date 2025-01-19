5b07a940d7009e5606f6e6749fc767caa3e070c7
Preeti Aarya <kumaripreetiaarya00@gmail.com>
2025-01-19 06:57:24 +0530

    increase the angle by 1 degree each frame

--- chap-3-oscillation/rectangleRotation.js
+++ chap-3-oscillation/rectangleRotation.js
@@ -1,11 +1,17 @@
+let angle = 2;  // Set the initial angle value, which will control the rotation of the rectangle
+
 function setup() {
-  createCanvas(400, 400);
-  angleMode(DEGREES);
+  createCanvas(400, 400);  // Create a 400x400 pixel canvas
+  angleMode(DEGREES);  // Set the angle mode to DEGREES (default is RADIANS)
 }
 
 function draw() {
-  background(220);
-  // rotate(45);
-  fill(255);
-  rect(50,50,100,50);
+  background(220, 0); 
+  rotate(angle);  // Rotate the canvas by the current value of 'angle'
+
+  fill(255); 
+  rect(200, 200, 100, 50);  // Draw a rectangle at (200, 200) position with width 100 and height 50
+
+  angle = angle + 1;  // Increase the 'angle' by 1 degree each frame to rotate the rectangle continuously
 }
+
