6806600403fde0eb2ac84393cf13bbe6973ecaae
Preeti Aarya <kumaripreetiaarya00@gmail.com>
2025-01-20 12:02:18 +0530

    Map mouseX position to control rotation speed and Constrain angular velocity

--- chap-3-Oscillation/angularMotion/angularMotion.js
+++ chap-3-Oscillation/angularMotion/angularMotion.js
@@ -8,5 +8,14 @@ function setup() {
 }
 
 function draw() {
+    angleA = map(mouseX,0,width,-0.01,0.01);
+    angleV = constrain(angleV, -0.2,0.2);
     background(220);
+    stroke(0);
+    fill(255);
+    rectMode(CENTER);
+
+    translate(200,200);
+    rotate(angle);
+    rect(0,0,250,25);
 }
