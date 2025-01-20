7719ccbf63b8c8203e18c7e1a6548e663b549fda
Preeti Aarya <kumaripreetiaarya00@gmail.com>
2025-01-20 11:56:05 +0530

    degfine a variable of angular motion and set angleMode in radians

--- chap-3-Oscillation/angularMotion/angularMotion.js
+++ chap-3-Oscillation/angularMotion/angularMotion.js
@@ -1,7 +1,12 @@
-function setup(){
-    createCanvas(400,400);
+let angle = 0;
+let angleV = 0;
+let angleA = 0;
+
+function setup() {
+    createCanvas(400, 400);
+    angleMode(RADIANS);
 }
 
-function draw(){
+function draw() {
     background(220);
 }
