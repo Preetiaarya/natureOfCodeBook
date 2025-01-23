147feac29c183e609d230434434b701c8e68975c
Preeti Aarya <kumaripreetiaarya00@gmail.com>
2025-01-23 11:29:18 +0530

    added key constants and increase or decreases car x coordinate

--- chap-3-Oscillation/carMovingByArrow/carMovingByArrow.js
+++ chap-3-Oscillation/carMovingByArrow/carMovingByArrow.js
@@ -1,6 +1,23 @@
-function setup(){
-    createCanvas(600,400);
+
+let carX = 15;  //Represents the car's horizontal position on the screen.
+function setup() {
+    createCanvas(600, 400);
 }
-function draw(){
+function draw() {
     background(220);
+
+    rect(carX, 200, 80, 50);
+    ellipse(carX, 250, 35, 35);
+    ellipse(carX + 75, 250, 35, 35);
+
+    // Check if the LEFT arrow key is pressed
+    if (keyCode == LEFT_ARROW) {
+        carX -= 4;  // Move the car left by reducing its x-coordinate
+    }
+
+    // Check if the RIGHT arrow key is pressed
+    if (keyCode == RIGHT_ARROW) {
+        carX += 4;  // Move the car right by increasing its x-coordinate
+    }
+
 }
