8dd3d752d590d99830c8d301bfaef9f7ab4496f3
Preeti Aarya <kumaripreetiaarya00@gmail.com>
2025-01-24 12:21:16 +0530

    Calculate the radius of the circle using a sine wave and Increment the angle to update the sine wave for the next frame

--- chap-3-Oscillation/harmonicMotion/harmonicMotion.js
+++ chap-3-Oscillation/harmonicMotion/harmonicMotion.js
@@ -1,10 +1,13 @@
+let angle = 0;
 function setup() {
     createCanvas(400, 400);
 }
 function draw() {
-    background(220);
+    background(0, 10);
     translate(200, 200);
-    fill("yellow");
-    let r = 150;
+    fill(252,238,33);
+    let r = sin(angle) * 200;
     circle(0, 0, r * 2);
+
+    angle += 0.1;
 }
