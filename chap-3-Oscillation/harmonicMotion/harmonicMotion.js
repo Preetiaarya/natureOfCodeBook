43fa6ae6b46e93fa4d9151c3d603aa21850a6c6f
Preeti Aarya <kumaripreetiaarya00@gmail.com>
2025-01-24 12:10:40 +0530

    draw a yellow circle which r is 150

--- chap-3-Oscillation/harmonicMotion/harmonicMotion.js
+++ chap-3-Oscillation/harmonicMotion/harmonicMotion.js
@@ -1,6 +1,10 @@
-function setup(){
-    createCanvas(400,400);
+function setup() {
+    createCanvas(400, 400);
 }
-function draw(){
+function draw() {
     background(220);
+    translate(200, 200);
+    fill("yellow");
+    let r = 150;
+    circle(0, 0, r * 2);
 }
