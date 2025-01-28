87f2747524821d0cb01b8c7ca9098fa01ee31998
Preeti Aarya <kumaripreetiaarya00@gmail.com>
2025-01-28 11:15:03 +0530

    added a circle with pink color

--- chap-3-Oscillation/harmonicMotion-y-axis/harmonicMotion-y-axis.js
+++ chap-3-Oscillation/harmonicMotion-y-axis/harmonicMotion-y-axis.js
@@ -1,6 +1,14 @@
+
 function setup(){
     createCanvas(600,400);
 }
 function draw(){
     background(220);
+    
+    translate(width/2,height/2);
+    stroke(0);
+    strokeWeight(2);
+    fill("pink");
+    // line();
+    circle(0,y,35);
 }
