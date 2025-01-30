630ebf6456007a18af51ab137f266604b3f07929
Preeti Aarya <kumaripreetiaarya00@gmail.com>
2025-01-30 12:19:50 +0530

    maping sine value to vertical pos and index to horizontal pos

--- chap-3-Oscillation/Graphing-sine-wave/Graphing-sine-wave.js
+++ chap-3-Oscillation/Graphing-sine-wave/Graphing-sine-wave.js
@@ -1,6 +1,27 @@
-function setup(){
-    createCanvas(800,600);
+let angles = [];
+let angleV = 0.05;
+let r = 16;
+function setup() {
+    createCanvas(800, 600);
+
+    let total = floor(width / (r * 2));
+    for (let i = 0; i < total; i++) {
+        angles[i] = 0;
+    }
 }
-function draw(){
-  background(220);
+function draw() {
+    background(0);
+
+    translate(width / 2, height / 2);
+    fill("yellow");
+    stroke("yellow");
+    for (let i = 0; i < angles.length; i++) {
+        let y = map(sin(angles[i]), -1, 1, -height / 2, height / 2);
+        strokeWeight(4);
+        line(0, 0, 0, y);
+        let x = map(i, 0, angles.length, -width / 2, width / 2);
+        circle(x, y, r * 2);
+
+        angles[i] += angleV;
+    }
 }
