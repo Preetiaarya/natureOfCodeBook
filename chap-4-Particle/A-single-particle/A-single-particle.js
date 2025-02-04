9a34fa42cbaba8577fdcd573bfb52f0d58d14915
Preeti Aarya <kumaripreetiaarya00@gmail.com>
2025-02-04 14:41:24 +0530

    added particle class to handle particle initial pos,veland acc

--- chap-4-Particle/A-single-particle/A-single-particle.js
+++ chap-4-Particle/A-single-particle/A-single-particle.js
@@ -1,6 +1,31 @@
-function setup(){
-    createCanvas(800,400);
+let system;
+
+function setup() {
+    createCanvas(800, 400);
+    system = new Particlesystem();
 }
-function draw(){
+function draw() {
     background(220);
+
+    system.update();
+    system.show();
+}
+
+class Particlesystem {
+    constructor(x, y) {
+        this.position = createVector(x, y);
+        this.velocity = createVector();
+        this.acceleration = createVector();
+    }
+    update() {
+        this.position.add(this.velocity);
+        this.velocity.add(this.acceleration);
+        this.acceleration.mult(0);
+    }
+    show() {
+        stroke(0);
+        translate(400,200);
+        fill(0);
+        circle(this.position.x, this.position.y, 20);
+    }
 }
