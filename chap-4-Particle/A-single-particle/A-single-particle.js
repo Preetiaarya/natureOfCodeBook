96c297cb70d0f9f696bc4b4e4cf256e6024cfd90
Preeti Aarya <kumaripreetiaarya00@gmail.com>
2025-02-04 14:57:13 +0530

    add gravity force and the particle alive or dead

--- chap-4-Particle/A-single-particle/A-single-particle.js
+++ chap-4-Particle/A-single-particle/A-single-particle.js
@@ -1,31 +1,57 @@
-let system;
+let particle;
 
 function setup() {
-    createCanvas(800, 400);
-    system = new Particlesystem();
+    createCanvas(600, 400);
+    particle = new Particle(width / 2, 15);
 }
+
 function draw() {
-    background(220);
+    background(220,0);
+    // Operating the single Particle
+    particle.update();
+    particle.show();
 
-    system.update();
-    system.show();
+    // Applying a gravity force
+    let gravity = createVector(0, 0.1);
+    particle.applyForce(gravity);
+
+    // Checking the particle's state and making a new particle
+    if (particle.isDead()) {
+        particle = new Particle(width / 2, 20);
+        console.log("Particle dead!");
+    }
 }
+class Particle {
 
-class Particlesystem {
     constructor(x, y) {
         this.position = createVector(x, y);
-        this.velocity = createVector();
-        this.acceleration = createVector();
+        //{!1 .offset-top} For demonstration purposes the Particle has a random velocity.
+        this.velocity = createVector(random(-1, 1), random(-2, 0));
+        this.acceleration = createVector(0, 0);
+        this.lifespan = 255.0;
     }
+
     update() {
-        this.position.add(this.velocity);
         this.velocity.add(this.acceleration);
+        this.position.add(this.velocity);
+        this.lifespan -= 2.0;
         this.acceleration.mult(0);
     }
+
+
     show() {
-        stroke(0);
-        translate(400,200);
-        fill(0);
-        circle(this.position.x, this.position.y, 20);
+        stroke(0, this.lifespan);
+        fill(0, 0, 255, this.lifespan);
+        circle(this.position.x, this.position.y, 15);
+    }
+
+    //{!3} Keeping the same physics model as with previous chapters
+    applyForce(force) {
+        this.acceleration.add(force);
+    }
+
+    //{!3} Is the Particle alive or dead?
+    isDead() {
+        return (this.lifespan < 0);
     }
 }
